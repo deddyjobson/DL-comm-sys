f7817b5da106a58343a83ba28c50b96f6c6a1e8c
Deddy Jobson <dedjob@hotmail.com>
2019-02-01 10:25:16 +0530

    tests single model over various snrs

--- Channel modulation/plotter.py
+++ Channel modulation/plotter.py
@@ -1,32 +1,88 @@
 import numpy as np
+import argparse
+import torch
+import pickle
 import os
+import errno
 import matplotlib.pyplot as plt
 
-digits = 7
+from shutil import copyfile
+from os.path import join
+from scipy.stats.mstats import gmean
 
-snr_dBs = np.linspace(-2,10,20)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--bs',type=int,default=32) # batch size
+parser.add_argument('--n',type=int,default=2) # encoding length
+parser.add_argument('--k',type=int,default=2) # number of bits
+parser.add_argument('--SNR',type=float,default=2.51188643150958) # signal to noise ratio
+# parser.add_argument('--SNR',type=float,default=1.5848931924611136) # signal to noise ratio
+parser.add_argument('--e_prec',type=int,default=6) # precision of error
+
+hyper = parser.parse_args()
+hyper.M = 2 ** hyper.k
+
+
+device = "cpu" # default
+
+def generate_input(amt=hyper.bs): # to generate inputs
+    indices = torch.randint(low=0,high=hyper.M,size=(amt,),device=device)
+    return indices,torch.eye(hyper.M,device=device)[indices]
+
+def accuracy(out, labels):
+  outputs = torch.argmax(out, dim=1)
+  return 100 * torch.sum(outputs==labels).to(dtype=torch.double)/labels.numel()
+
+def error_rate(out,labels):
+    return 1 - accuracy(out,labels)/100
+
+
+encoder = torch.load( join('Best','best_encoder({1},{2})_{0}.pt'.format(hyper.SNR,hyper.n,hyper.k)) )
+decoder = torch.load( join('Best','best_decoder({1},{2})_{0}.pt'.format(hyper.SNR,hyper.n,hyper.k)) )
+encoder.to(device)
+decoder.to(device)
+model = torch.nn.Sequential(encoder,decoder) # end to end model
+model.to(device)
+
+
+# snr_dBs = np.linspace(-2, 10, 25)
+# snr_dBs = np.linspace(-4, 8, 25)
+low = -2
+up = 10
+
+snr_dBs = np.linspace(low,up,25)
 snrs = 10 ** (snr_dBs/10)
 
+errs = np.zeros_like(snrs)
 
-if False:
-    if os.path.isfile('results.csv'):
-        os.remove('results.csv')
-
-    for snr in snrs:
-        hyper_lst = [500,2,2,0,snr,digits,1]
-        hyper_str = '--n_epochs {0} --M {1} --n {2} --verbose {3} --SNR {4} --e_prec {5} --write_out {6}'.format(*hyper_lst)
-        os.system('python3 autoencoder.py '+hyper_str)
+for i,snr in enumerate(snrs):
+    print(i)
+    # scaler = np.sqrt( snr )
+    scaler = np.sqrt( snr * 2 * hyper.k / hyper.n )
 
 
-data = np.loadtxt(open("results.csv", "rb"), delimiter=",", skiprows=0)
-x,y = data.T
+    labels,ip = generate_input(amt=10**hyper.e_prec)
+    enc = encoder(ip)
+    enc = enc + torch.randn_like(enc,device=device) / scaler
+    op = decoder(enc)
 
-xx = x
-yy = y + 10**(-digits) # to soften the graph
+    errs[i] = error_rate(op,labels)
 
-plt.figure(dpi=300)
-plt.semilogy(xx,yy)
-plt.title('Error profile of autoencoder')
+
+xx = snr_dBs
+yy = errs + 1 / 10**hyper.e_prec # to protect against anomalous behaviour
+
+plt.figure(dpi=250)
+axes = plt.gca()
+axes.set_xlim([low,up])
+axes.set_ylim([1e-5,1e0])
+plt.semilogy(xx,yy,'-b')
+plt.semilogy(xx,yy,'or')
+plt.title('Error profile of autoencoder ({0},{1})'.format(hyper.n,hyper.k))
 plt.xlabel('$E_b/N_0$ [dB]')
 plt.ylabel('Block Error Rate')
+plt.grid()
+plt.savefig('error_rates_({0},{1}).png'.format(hyper.n,hyper.k))
 plt.show()
+
+exit()
