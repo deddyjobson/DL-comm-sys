150fb63d57a721388408de7a87b3dde3f3bbd07b
Deddy Jobson <dedjob@hotmail.com>
2019-03-17 18:40:57 +0530

    working with real data

--- Modulation recognition/benchmark.py
+++ Modulation recognition/benchmark.py
@@ -1,13 +1,29 @@
 import numpy as np
 import tensorflow as tf
 import os
+import pickle
 
 from time import time
 
+
+input_shape = (1,2,128)
+num_pts = 1000
+
+# loading dataset
+Xd = pickle.load(open(os.path.join("Data","RML2016.10a_dict.pkl"),'rb'))
+snrs,mods = map(lambda j: sorted(list(set(map(lambda x: x[j], Xd.keys())))), [1,0])
+X = []
+# lbl = []
+for mod in mods:
+    for snr in snrs:
+        X.append(Xd[(mod,snr)])
+X = np.vstack(X)[:num_pts].reshape((-1,*input_shape)).astype(np.float32)
+
+print('Measuring time taken over {0} samples...'.format(X.shape[0]))
 # Load TFLite model and allocate tensors.
 for model in ['teacher','student','student8']:
+    total_time = 0
     interpreter = tf.lite.Interpreter(model_path=os.path.join('Best',"{0}.tflite".format(model)))
-    # interpreter = tf.lite.Interpreter(model_path=os.path.join('Best',"student.tflite"))
     interpreter.allocate_tensors()
 
     # Get input and output tensors.
@@ -15,12 +31,12 @@ for model in ['teacher','student','student8']:
     output_details = interpreter.get_output_details()
 
     # Test model on random input data.
-    input_shape = input_details[0]['shape']
-    input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-    interpreter.set_tensor(input_details[0]['index'], input_data)
+    for i in range(X.shape[0]):
+        interpreter.set_tensor(input_details[0]['index'], X[i])
 
-    start = time()
-    interpreter.invoke() # run through model
+        start = time()
+        interpreter.invoke() # run through model
+        total_time += time() - start
     # output_data = interpreter.get_tensor(output_details[0]['index'])
     # print(output_data)
-    print('Time taken by {1} :{0:.2f}ms'.format(10**3 * (time()-start) , model))
+    print('Time taken by {1} :{0:.2f}s'.format(total_time , model))
